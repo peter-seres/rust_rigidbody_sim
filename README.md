2e3a6485a5b5c317e3e193bf77cf583a6d9a2e6c
Speterius <peet.seres@gmail.com>
2020-04-15 03:22:06 +0200

    Added gamepad support.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # rust_rigidbody_sim
-Real-time simulation for a 6DOF rigidbody, 
+Real-time simulation for a 6DOF rigidbody. Implemented for a drone using NED coordinate system and quaternion attitude representation.
+
+## Features:
+- rigid body with parameters: m, I and 6 degrees of freedom
+- 3d visuals using Kiss3d
+- support for a gamepad controller
+
+### Rust installation guide:
+
+https://www.rust-lang.org/tools/install
+
+ // On Windows: Visual Studio c++ Build Tools are needed. // 
+
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
 extern crate nalgebra as na;
 extern crate kiss3d;
 #[macro_use] extern crate log;
+use gilrs::{Gilrs, Axis, Event, Gamepad, EventType};
 use std::fmt;
 use std::time;
 use std::thread;
 use std::path::Path;
 
-// Todo list:
-// 2. xbox joystick input handling
-// 3. plotting / logging
-// 4. add runge kutta dynamics
-// 5. add .obj file for pretty-ness
 
+// Todo list:
+// 1. refactor and organize
+// 2. plotting / logging
+// 3. add runge kutta dynamics
+
+
+// For quaternion rotations:
 fn as_matrix(q: na::Vector4<f32>) -> na::Matrix4<f32> {
     let mat = na::Matrix4::new(q[0], -q[1], -q[2], -q[3],
                                q[1],  q[0], -q[3],  q[2],
@@ -20,6 +23,7 @@ fn as_matrix(q: na::Vector4<f32>) -> na::Matrix4<f32> {
     return mat;
 }
 
+// For inverse quaternion rotations:
 fn as_inverse_matrix(q: na::Vector4<f32>) -> na::Matrix4<f32> {
     let mat = na::Matrix4::new(q[0],   q[1],  q[2],  q[3],
                                -q[1],  q[0],  q[3], -q[2],
@@ -28,12 +32,13 @@ fn as_inverse_matrix(q: na::Vector4<f32>) -> na::Matrix4<f32> {
     return mat;
 }
 
-
+// Prepend 0 to Vector3:
 fn as_quat(v: na::Vector3<f32>) -> na::Vector4<f32> {
     let v_ext = na::Vector4::new(0.0, v[0], v[1], v[2]);
     return v_ext;
 }
 
+// Rotate vector p using quaternion:  p' = q p q.inv()
 fn rotate_vec(q: na::Vector4<f32>, p: na::Vector3<f32>) -> na::Vector3<f32> {
     let p_extended = as_quat(p);
     let p_rot_extended = as_matrix(q) * (as_matrix(p_extended) * as_inverse_matrix(q));
@@ -41,7 +46,8 @@ fn rotate_vec(q: na::Vector4<f32>, p: na::Vector3<f32>) -> na::Vector3<f32> {
     return p_rot;
 }
 
-fn rotate_quat(q_1: na::Vector4<f32>, q_2: na::Vector4<f32>) -> na::Vector4<f32> {
+// Rotate quaternion q_2 by q_1:
+fn _rotate_quat(q_1: na::Vector4<f32>, q_2: na::Vector4<f32>) -> na::Vector4<f32> {
     let q = as_matrix(q_1) * q_2;
     return q;
 }
@@ -123,6 +129,7 @@ impl fmt::Display for RigidBody
     }
 }
 
+// Time management: keeps track of loop times
 struct TimeStep 
 {
     last_time: time::Instant,
@@ -168,35 +175,86 @@ impl TimeStep {
 }
 
 
+const PI: f32 = 3.14159265359;
+
 fn main() 
 {
     // GUI Setup:
     let mut window  = kiss3d::window::Window::new("RigidBody Rust Demo by PS");
+    window.set_light(kiss3d::light::Light::StickToCamera);
+    let mut is_running = true;
 
-
-    //let mut cube    = window.add_cube(0.9, 0.3, 0.8);
+    // Drone object setup:
     let obj_path = Path::new("assets/aera_low_poly.obj");
     let mtl_path = Path::new("assets/");
     let mut cube = window.add_obj(&obj_path, &mtl_path, na::Vector3::new(0.6, 0.6, 0.6));
-
-
-    window.set_light(kiss3d::light::Light::StickToCamera);
     cube.set_color(0.6, 0.6, 0.93);
     let mut cube_trans: na::Translation3<f32>;
     let mut cube_rot: na::UnitQuaternion<f32>;
-    let mut is_running = true;
 
-    // Transformation matrices form NED to OpenGL coorinate system:
+    // Ground plane setup:
+    let mut quad = window.add_quad(13.0, 13.0, 10, 10);
+    quad.set_color(0.3, 0.3, 0.3);
+    quad.set_local_translation(na::Translation3::from(na::Vector3::new(0.0, -1.0, 0.0)));
+    quad.set_local_rotation(na::UnitQuaternion::from_euler_angles(PI/2.0, 0., 0.));
+
+    // Transformation matrices from NED to OpenGL coorinate system:
     let translation_transform: na::Matrix3<f32> = na::Matrix3::new(1., 0., 0., 0., 0., -1., 0., 1., 0.);
     let rotation_transform: na::Matrix4<f32> = na::Matrix4::new(0., 1., 0., 0., 0., 0., 0., -1., 0., 0., 1., 0., 1., 0., 0., 0.);
 
     // Rigidbody:
     let mut rb = RigidBody::build_default();
 
+    // Game pad management:
+    let mut gilrs = Gilrs::new().unwrap();
+    let mut _gp: Option<Gamepad> = None;
+
+    for (_id, gamepad) in gilrs.gamepads() {
+        println!(" >>> Available input device: {}", gamepad.name());
+        _gp = Some(gilrs.gamepad(gamepad.id()));
+    }
+
     // Time management:
     let mut time_manager = TimeStep::new();
+
+    let mut _throttle = 0.0;
+    let mut roll = 0.0;
+    let mut pitch = 0.0;
+    let mut yaw = 0.0;
+
     while is_running
     {
+
+        // Gamepad input:
+        while let Some(Event { id: _, event, time: _ }) = gilrs.next_event() {
+            match event {
+                EventType::AxisChanged(Axis::LeftStickX, val, _) => {
+                    println!("Left X {}", val);
+                    yaw = val; },
+                EventType::AxisChanged(Axis::LeftStickY, val, _) => {
+                    println!("Right Y {}", val);
+                    pitch = -val; },
+                EventType::AxisChanged(Axis::RightStickX, val, _) => {
+                    println!("Right X {}", val);
+                    roll = val; },
+                EventType::AxisChanged(Axis::RightStickY, val, _) => {
+                    println!("Right Y {}", val);
+                    pitch = -val; },
+                EventType::AxisChanged(Axis::DPadX, _, _) => (),
+                EventType::AxisChanged(Axis::DPadY, _, _) => (),
+                EventType::AxisChanged(Axis::LeftZ, _, _) => (),
+                EventType::AxisChanged(Axis::RightZ, _, _) => (),
+                EventType::AxisChanged(Axis::Unknown, _, _) => (),
+                EventType::ButtonPressed(_, _) => (),
+                EventType::ButtonChanged(_, _, _) => (),
+                EventType::ButtonRepeated(_, _) => (),
+                EventType::ButtonReleased(_, _) => (),
+                EventType::Connected => (),
+                EventType::Disconnected => (),
+                EventType::Dropped => (),
+            }
+        }
+
         // Let the Thread sleep for a bit:
         let sleepy_time = time::Duration::from_micros(500);
         thread::sleep(sleepy_time);
@@ -204,7 +262,7 @@ fn main()
 
         // Apply forces and moments in the body frame:
         let forces = na::Vector3::new(0.0, 0.0, 0.0);
-        let moments = na::Vector3::new(0.0, 0.0, 0.0);
+        let moments = na::Vector3::new(roll, pitch, yaw);
 
         // Rigid body step forward:
         rb.step(forces, moments, dt);
@@ -212,9 +270,8 @@ fn main()
         // Render only every MS_PER_FRAME millisecond: 
         if time_manager.should_render() {
 
-            // Move the cube:
+            // Move drone in the scene:
             let cube_cg = translation_transform * rb.position;
-
             cube_trans= na::Translation3::from(cube_cg);
             cube_rot = na::UnitQuaternion::from_quaternion(na::Quaternion::from(rotation_transform * rb.orientation));
             cube.set_local_translation(cube_trans);
@@ -235,7 +292,6 @@ fn main()
             window.draw_line(&na::Point3::from(cube_cg), 
                              &na::Point3::from(cube_cg + translation_transform * (rotate_vec(rb.orientation, na::Vector3::new(0., 0., 1.2)))),
                              &na::Point3::new(0., 0., 1.));
-
         }
     }
 }
