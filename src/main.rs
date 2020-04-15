d89521dc8a382bd70a5f5835911ee684a00ed3e6
Speterius <peet.seres@gmail.com>
2020-04-15 22:53:17 +0200

    a few comments and adjusting the loop time.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 extern crate nalgebra as na;
-
 use std::time;
 use std::thread;
-
 mod quaternion;
 mod rigid_body;
 mod input_manager;
@@ -23,30 +21,24 @@ fn main()
     // Gui scene setup: (contains the main window, the uav SceneNode and the ground plane SceneNode)
     let mut scene = scene_manager::MyScene::new();
 
-    // Rigidbody:
+    // Rigidbody: (contains all states and 6dof dynamics)
     let mut rb = rigid_body::RigidBody::new();
 
-    // Game pad management:
+    // Game pad management (contains: manual control struct from joystick)
     let mut inputs = input_manager::ControllerInput::new();
 
-    // Gamepad inputs:
-    let mut _throttle = 0.0;
-    let mut _roll = 0.0;
-    let mut _pitch = 0.0;
-    let mut _yaw = 0.0;
-
-    // Time management:
+    // Time management (keeps track of loop time and render time)
     let mut time_manager = time_manager::TimeStep::new();
 
     // Main Loop:
     let mut is_running = true;
-    while is_running
-    {
+    while is_running {
+
         // Gamepad input:
         inputs.listen_to_inputs();
 
         // Let the Thread sleep for a bit:
-        let sleepy_time = time::Duration::from_micros(500);
+        let sleepy_time = time::Duration::from_micros(1000);
         thread::sleep(sleepy_time);
         let dt = time_manager.delta();
 
